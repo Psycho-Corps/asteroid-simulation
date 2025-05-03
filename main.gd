89bc34b34048721aff24c1f1a6ec9a91775e3708
MasterChaosGames <forgottenmercenarygames@gmail.com>
2025-05-02 23:23:25 -0500

    Different Asteroid Types
    Created different asteroid sizes (big, medium, small) and. when destroyed asteroids will split into these three types. The asteroids have a 1.5 sec immunity timer to prevent asteroids from being destroyed by each other when spawning from parent asteroid.

--- main.gd
+++ main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var window_size : Vector2
 var i = 0
-var NUM_ASTEROIDS = 100
+var NUM_ASTEROIDS = 50
 var asteroid_scene : PackedScene = preload("res://asteroid-simulation/scenes/Asteroid.tscn")
 
 # Called when the node enters the scene tree for the first time.
@@ -12,21 +12,43 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	while i < NUM_ASTEROIDS:
-		spawn_asteroids()
+		spawn_asteroids("Big", Vector2(0,0))
 		i += 1
-	
-func spawn_asteroids() -> void:
+						
+func spawn_asteroids(group : String, s_position : Vector2) -> void:
 	var asteroid = asteroid_scene.instantiate()
-	asteroid.body_entered.connect(_on_body_entered)
-	var random_position = Vector2(randf_range(0, window_size.x), randf_range(0, window_size.y))
-	asteroid.global_position = random_position
-	add_child(asteroid)
-
+	var components = asteroid.find_child("Components")
+	var health_component = components.find_child("Health")
+	match group:
+		"Big":
+			health_component.hp = 30
+			var random_position = Vector2(randf_range(0, window_size.x), randf_range(0, window_size.y))
+			asteroid.global_position = random_position
+			asteroid.add_to_group("Big")
+		"Medium":
+			health_component.hp = 20
+			asteroid.modulate = Color(1, 0, 1, 1)
+			asteroid.global_position = s_position
+			asteroid.add_to_group("Medium")
+		"Small":
+			health_component.hp = 10
+			asteroid.modulate = Color(0, 1, 1, 1)
+			asteroid.global_position = s_position
+			asteroid.add_to_group("Small")
+	asteroid.call_deferred("connect", "body_entered", _on_body_entered)
+	call_deferred("add_child", asteroid)
+			
 func _on_body_entered(body: Node) -> void:
 	if body.has_node("Components"):
 		var components = body.find_child("Components")
 		if components.find_child("Health"):
 			if components.find_child("Health").hp <= 0:
+				if body.is_in_group("Big"):
+					for _i in range(2):
+						spawn_asteroids("Medium", body.global_position)
+				elif body.is_in_group("Medium"):
+					for _i in range(4):
+						spawn_asteroids("Small", body.global_position)
 				body.queue_free()
 			else:
 				components.find_child("Health").hp -= 10
--- systems/asteroid.gd
+++ systems/asteroid.gd
@@ -1,14 +1,20 @@
 extends RigidBody2D
 
+@onready var immunity_timer = $Timer
 var random_velocity = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	gravity_scale = 0
 	
+	set_collision_layer_value(1, false)
+	set_collision_mask_value(1, false)
+	
 	random_velocity = Vector2(randf_range(-100, 100), randf_range(-100, 100))
 	linear_velocity = random_velocity
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if immunity_timer.is_stopped():
+		set_collision_layer_value(1, true)
+		set_collision_mask_value(1, true)
